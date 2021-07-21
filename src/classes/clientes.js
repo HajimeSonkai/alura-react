722390f88d751a22fcab3d69a8b7988c2b1c43da
sonkai <mfilho86@gmail.com>
2021-07-21 20:27:56 -0300

    aula 2

---
+++ src/classes/clientes.js
@@ -0,0 +1,32 @@
+class Cliente{
+    nome;
+    cpf;
+};
+
+class ContaCorrente {
+    agencia;
+    saldo;
+};
+
+const cliente1 = new Cliente();
+
+cliente1.nome = "ricardo";
+cliente1.cpf = 11122233309;
+// cliente1.agencia = 1001;
+// cliente1.saldo = 0;
+
+const cliente2 = new Cliente();
+cliente2.nome = "alice";
+cliente2.CPF = 88822233309;
+// cliente2.Agencia = 1001;
+// cliente2.Saldo = 0;
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.saldo = 100;
+console.log(contaCorrenteRicardo.saldo);
+
+console.log(cliente1);
+console.log(cliente2);
